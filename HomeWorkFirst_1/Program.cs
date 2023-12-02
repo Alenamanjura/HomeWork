56ebaf2d2f24bda1bbfbdbee21f6e24936319d86
Alena Manjura <alenamanjura@yandex.ru>
2023-12-02 23:50:12 +0300

    add home work 1

---
+++ HomeWorkFirst_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int firstNumder = int.Parse(Console.ReadLine()!);
+if(firstNumder % 161 == 0)
+ Console.WriteLine($"Число {firstNumder} кратно одновременно 7 и 23");
+else {
+                    Console.WriteLine($"Число {firstNumder} не кратно одновременно 7 и 23");
+}
