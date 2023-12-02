e717d43ceef9bd5d10602cc66c205de6ccf161fa
Alena Manjura <alenamanjura@yandex.ru>
2023-12-02 22:50:02 +0300

    add home work 4

---
+++ HomeWorkFirst_04/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите натуральное число: ");
+string inputNum = (Console.ReadLine()!);
+int n = int.Parse(inputNum);
+string outNumLine = string.Empty;
+int count = 0;
+
+while(count < n){
+    outNumLine = outNumLine + count + ",";
+    count = +1;
+}
+
+Console.WriteLine(outNumLine + n);
+
