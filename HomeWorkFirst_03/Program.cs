c234ce96c20f2bcb39105002dd7d740b6a93d1a7
Alena Manjura <alenamanjura@yandex.ru>
2023-12-02 22:49:40 +0300

    add home work 3

---
+++ HomeWorkFirst_03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число от отрезка [10, 99]: ");
+int number = int.Parse(Console.ReadLine()!);
+
+
+while(number < 10 || number > 99){
+Console.WriteLine($"Ошибка: Вы ввели числа меньше 10 либо больше 99.\n Попробуйте снова");
+number = int.Parse(Console.ReadLine()!);
+}
+if (number > 10 || number < 99){
+                    
+          Console.WriteLine("Наибольшая цыфра этого числа: " + number / 10);          
+}
+else {
+Console.WriteLine("Наибольшая цыфра этого числа: "+ number % 10);
+}
