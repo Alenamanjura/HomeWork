45b53d78e0c505eaece458e87f67ae310b435b0a
Alena Manjura <alenamanjura@yandex.ru>
2023-12-02 22:49:11 +0300

    add home work 2

---
+++ HomeWorkFirst_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+static void Main()
+{
+Console.Clear();
+Console.Write("Введите координаты точки X и Y через пробел, не равные нулю: ");
+int[] coordinates = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int x =  Convert.ToInt32(coordinates[0]);
+int y =  Convert.ToInt32(coordinates[1]);
+
+if(x == 0 || y == 0){
+                    Console.WriteLine("Ошибка: X и Y должны быть не равные нулю.");
+}
+
+if (x > 0 && y > 0)
+Console.WriteLine("1");
+else if (x < 0 && y > 0)Console.WriteLine("2");
+else if (x < 0 && y < 0)
+Console.WriteLine("3");
+else if (x > 0 && y < 0)
+Console.WriteLine("4");
+else
+Console.WriteLine("Точка находится на оси координат: ");
+}
+}
+
